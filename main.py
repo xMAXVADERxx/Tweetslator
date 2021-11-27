4efc7c25c1d59208be35f102608880d2dbc5b101
xMAXVADERxx <max.pearce47@gmail.com>
2021-11-27 14:10:41 +0000

    Integrated Twitter and Translate, it works!

--- main.py
+++ main.py
@@ -1,39 +1,87 @@
 import tweepy
 import dotenv
 from googletrans import Translator
+import datetime
 import random
+import time
 
 translator = Translator()
 
 file = open("GoogleCodes", "r")
 codes = file.readlines()
+print(codes)
 for x in range(len(codes)):
     codes[x] = codes[x].strip()
+print(codes)
 
-def randTrans(str, list):
-    translation = translator.translate(str, dest=list[random.randint(0,len(list)-1)])
+def trans(str, destLang=None):
+    if destLang == None:
+        destLang = codes[random.randint(0,len(codes)-1)]
+    print(f"Destination Language Code: {destLang}")
+    translation = translator.translate(str, dest=destLang)
     return translation
 
 
-#get access tokens
+#get access tokens and config data
 config = dotenv.dotenv_values(".env")
 CONSUMER_KEY = config["CONSKEY"]
 CONSUMER_SECRET = config["CONSSECRET"]
 ACCESS_TOKEN = config["ACCTOK"]
 ACCESS_TOKEN_SECRET = config["ACCTOKSEC"]
 BEARER = config["BEARER"]
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-api = tweepy.API(auth)
+ID = config["SELF_ID"]
+try:
+    updateTime = config["FIRSTTIME"]
+except all:
+    updateTime = "2020-01-01T00:00:00Z"
 
 #create a client
 client = tweepy.Client(BEARER, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+#uncomment to test client setup VV
 #client.create_tweet(text="Test Tweet, pls ignore")
 
+user = client.get_user(id=ID)
 
-
-translation = randTrans("Hello, there!", codes)
+translation = trans("Hello, there!")
 print(translation.text)
 
+while True:
+    
+    #gets user mentions and tweets
+    try:
+        mentions = client.get_users_mentions(id=ID, start_time=updateTime)
+
+    except tweepy.TweepyException:
+        mentions = ["Empty"]
+        print(mentions)
+
+    try:
+        tweets = client.get_users_tweets(id=ID)
+    except tweepy.TweepyException:
+        #if tweet can't be found, just grab my most recent tweet (@xmaxvaderxx, btw)
+        tweets = client.get_users_tweets(id=2365430914, max_results=1)
+
+    #outputs information to console
+    print(f"{str(len(mentions[0]))} tweets mention @{user[0].name}")
+    print(mentions)
+    
+
+    #doing stuff with tweets
+    for mention in mentions[0]:
+        print(mention)
+        if (not (mention in tweets[0])) and (mentions[0] != "Empty" ):
+            text = mention.text
+            
+            #remove handle, frees up characterss
+            text.replace("@TweetslatorBot","")
+            translation = trans(text)
+            #client.create_tweet(text=translation.text, in_reply_to_tweet_id=mention.id)
+
+    #sets new timeframe, in order to prevent scanning and replying to already seen tweets
+    date = datetime.datetime.now()
+    updateTime = f"{str(date.year)}-{str(date.month)}-{str(date.day)}T{str(date.hour)}:{str(date.minute)}:{str(date.second)}Z"
+    print(f"Update time: {updateTime}")
+
+    #wait - saves system from dying and keeps bot within API limits
+    time.sleep(15)
